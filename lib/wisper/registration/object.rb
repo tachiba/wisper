4462efb46b15625355a034293a497907e26ee04b
Kris Leech <kris.leech@gmail.com>
2014-01-04 00:23:48 +0000

    wip: Allow listener to be subscribed to all instances of a class [#31]
    * passing spec
    * implementation

--- lib/wisper/registration/object.rb
+++ lib/wisper/registration/object.rb
@@ -1,22 +1,28 @@
 module Wisper
   class ObjectRegistration < Registration
-    attr_reader :with, :prefix
+    attr_reader :with, :prefix, :scope
 
     def initialize(listener, options)
       super(listener, options)
       @with   = options[:with]
       @prefix = stringify_prefix(options[:prefix])
+      @scope = options.fetch(:scope, :all)
       fail_on_async if options.has_key?(:async)
     end
 
-    def broadcast(event, *args)
+    def broadcast(event, publisher, *args)
       method_to_call = map_event_to_method(event)
-      if should_broadcast?(event) && listener.respond_to?(method_to_call)
+      if should_broadcast?(event) && listener.respond_to?(method_to_call) && in_scope?(publisher)
         listener.public_send(method_to_call, *args)
       end
     end
 
     private
+
+    def in_scope?(publisher)
+      return true if scope == :all
+      publisher.class == scope
+    end
 
     def map_event_to_method(event)
       prefix + (with || event).to_s
